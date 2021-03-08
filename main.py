0573c458bb2245cdc1580bde519d8c71283dc3c0
mohammad <mozafari.mmd@gmail.com>
2021-03-08 18:45:18 +0330

    Use DBSCAN for clusteing (not good)

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import torch
 import numpy as np
+from sklearn.cluster import DBSCAN
 from utils.detection import Detection
 from torch.utils.data import DataLoader
 from utils.face_dataset import FaceDataset
 from utils.utility_functions import get_images_paths
 from utils.inception_resnet_v1 import InceptionResnetV1
 
-paths = get_images_paths('.\\data')
-d = Detection('./results/bounding_boxes.csv')
-faces = d.detect_faces(paths)
-print('Faces are detected')
+# paths = get_images_paths('.\\data')
+# d = Detection('./results/bounding_boxes.csv')
+# faces = d.detect_faces(paths)
+# print('Faces are detected')
 
 face_ds = FaceDataset('./results/bounding_boxes.csv')
 batch_size = 2
@@ -23,5 +24,6 @@ for i, x in enumerate(dl):
         y = net(x)
     embeddings[i*batch_size:i*batch_size+x.shape[0], :] = y
 
-dist = np.sum(embeddings ** 2, axis=1).reshape(-1, 1) + np.sum(embeddings ** 2, axis=1).reshape(1, -1) - 2 * (embeddings @ embeddings.T)
-print(dist < 1.1)
+alg = DBSCAN(eps=1.1, min_samples=2)
+labels = alg.fit(embeddings).labels_
+print(labels)
