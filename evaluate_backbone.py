d25fbb074abd07b058c90e259b78fb127c395e59
mohammad <mozafari.mmd@gmail.com>
2021-04-08 14:44:33 +0430

    Set checkpoint

--- evaluate_backbone.py
+++ evaluate_backbone.py
@@ -13,4 +13,22 @@ def extract_features():
     paths = fe.extract_features()
     return paths
 
-extract_features()
+def evaluate_results():
+    pairs_files = './data/pairs.txt'
+    with open(pairs_files) as f:
+        lines = f.readlines()
+        for line in lines:
+            parts = line.split()
+            target, name1, name2 = None, None, None
+            if len(parts) == 3:
+                target = 1
+                name1 = '{}_{:0>4d}.jpg'.format(parts[0], int(parts[1]))
+                name2 = '{}_{:0>4d}.jpg'.format(parts[0], int(parts[2]))
+            elif len(parts) == 4:
+                target = 0
+                name1 = '{}_{:0>4d}.jpg'.format(parts[0], int(parts[1]))
+                name2 = '{}_{:0>4d}.jpg'.format(parts[2], int(parts[3]))
+            
+
+# extract_features()
+evaluate_results()
